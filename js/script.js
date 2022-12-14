471b9f4176b4f6c4aaa0f21787b2713f5ab266de
kaia <111490006+kaia-dan@users.noreply.github.com>
2022-12-14 10:18:22 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,81 @@
+ //1.스크립트 위로 튕기는것 (a태그 눌렀을때 위로 튕기는거 막아줌)
+$(document).on('click', 'a[href="#"]', function(e){
+    e.preventDefault();
+});
+
+//2.슬라이드
+$(function() {  //$(function() {});  jquery를 호출하겠다.
+    $(".visual .visual_list").slick({  //$("적용해줄 클래스명").slick- 선택한 클래스에 slick을 적용하겠다
+        arrows: false, //화살표 사용안함
+        dots: false, //닷츠
+        autoplay: true, //자동재생
+        fade: true, //페이드인 효과        
+        autoplaySpeed: 3000, //재생시간
+        pauseOnHover:false, //마우스 오버시 슬라이드 멈춤 해제
+        pauseOnFocus:false //포커스시 슬라이드 멈춤 해제
+    });
+}); 
+
+
+$(function() {  //$(function() {});  jquery를 호출하겠다.
+    $(".our_sec .our_slide").slick({  //$("적용해줄 클래스명").slick- 선택한 클래스에 slick을 적용하겠다
+        arrows: false, //화살표 사용안함
+        dots: false, //닷츠
+        autoplay: true, //자동재생
+        infinite: true,
+        // fade: true, //페이드인 효과        
+        autoplaySpeed: 2500, //재생시간
+        pauseOnHover:false, //마우스 오버시 슬라이드 멈춤 해제
+        pauseOnFocus:false, //포커스시 슬라이드 멈춤 해제
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        draggable: true,
+        centerMode:true,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode:false
+
+                }
+            }
+        ]
+    });
+}); 
+
+$(function() {  //$(function() {});  jquery를 호출하겠다.
+    $(".bafter_sec .bafter_list").slick({  //$("적용해줄 클래스명").slick- 선택한 클래스에 slick을 적용하겠다
+        arrows: false, //화살표 사용안함
+        dots: false, //닷츠
+        autoplay: true, //자동재생
+        infinite: true,
+        // fade: true, //페이드인 효과        
+        autoplaySpeed: 2500, //재생시간
+        pauseOnHover:false, //마우스 오버시 슬라이드 멈춤 해제
+        pauseOnFocus:false, //포커스시 슬라이드 멈춤 해제
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        draggable: true,
+        // centerMode:true,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+}); 
+
+//.3.scroll 애니메이션
+$(function() {
+	$('.animate').scrolla({
+		mobile: true, //모바일버전시 활성화
+		once: true //스크롤시 딱 한번만 하고싶을땐 true
+	});    
+}); 
